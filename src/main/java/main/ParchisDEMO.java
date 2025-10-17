ed8dd0b504160db2cd77327e68a634e481469b5d
ManfredLopez <manfred.lopezb@gmail.com>
2025-10-17 14:24:30 -0600

    Hello World

---
+++ src/main/java/main/ParchisDEMO.java
@@ -0,0 +1,16 @@
+package main;
+
+import view.GUIInicio;
+
+/**
+ *
+ * @author mjlop
+ */
+public class ParchisDEMO {
+
+    public static void main(String[] args) {
+        GUIInicio v1 = new GUIInicio();
+        v1.setVisible(true);
+        v1.setLocationRelativeTo(null);
+    }
+}
